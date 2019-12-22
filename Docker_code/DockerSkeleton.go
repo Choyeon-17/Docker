e635a1b73305029de97c767c44a9a3733e30e69b
Choyeon-17 <vtkohcawsmktv@gmail.com>
2019-12-22 18:11:14 +0900

    CREATE: Docker skeleton code

--- Docker_code/DockerSkeleton.go
+++ Docker_code/DockerSkeleton.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
-	"syscall"
 )
 
 func main() {
@@ -14,38 +13,30 @@ func main() {
 	case "child":
 		child()
 	default:
-		panic("wat should I do")
+		panic("what should I do")
 	}
 }
 
 func parent() {
 	cmd := exec.Command("/proc/self/exe", append([]string{"child"}, os.Args[2:]...)...)
-	cmd.SysProcAttr = &syscall.SysProcAttr{
-		Cloneflags: syscall.CLONE_NEWUTS | syscall.CLONE_NEWPID | syscall.CLONE_NEWNS,
-	}
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 
 	if err := cmd.Run(); err != nil {
-		fmt.Println("ERROR", err)
+		fmt.Println("error", err)
 		os.Exit(1)
 	}
 }
 
 func child() {
-	must(syscall.Mount("rootfs", "rootfs", "", syscall.MS_BIND, ""))
-	must(os.MkdirAll("rootfs/oldrootfs", 0700))
-	must(syscall.PivotRoot("rootfs", "rootfs/oldrootfs"))
-	must(os.Chdir("/"))
-
 	cmd := exec.Command(os.Args[2], os.Args[3:]...)
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 
 	if err := cmd.Run(); err != nil {
-		fmt.Println("ERROR", err)
+		fmt.Println("error", err)
 		os.Exit(1)
 	}
 }
